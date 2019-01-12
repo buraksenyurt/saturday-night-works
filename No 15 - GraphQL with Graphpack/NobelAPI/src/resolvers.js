6e74cc571dc27eed6caf551d39a09f326275104a
buraksenyurt <burakselimsenyurt@gmail.com>
2019-01-12 21:00:45 +0300

    Mutaion kodları yazıldı ve yeni Query tipi eklendi

--- No 15 - GraphQL with Graphpack/NobelAPI/src/resolvers.js
+++ No 15 - GraphQL with Graphpack/NobelAPI/src/resolvers.js
@@ -1,4 +1,6 @@
-// Sorguları işleten modülümüz. Pek tabii laureate veritabanını kullanıyor
+// Sorguları işleten modülümüz. 
+// Pek tabii sözde laureate veritabanını kullanıyor
+// Veritabanı olan JSON dosyası array olarak belleğe yüklenir. Dolayısıyla uygulama sonlandığında ilgili değişiklikler kalıcı olarak dosyaya yazılmadıysa, kaybolacaklardır.
 import { laureates  } from "./laureate";
 
 const resolvers = {
@@ -12,6 +14,50 @@ const resolvers = {
     // ikinci sorgumuz ise id değerine göre arama yapıp sonucunu döndürmekte
     oneLaureate: (parent, { id }, context, info) => {
       return laureates.find(l => l.id === id);
+    },
+    // Yıllara göre nobel ödülü sahiplerinin listesini verir
+    laureatesByYear:(parent,{year},context,info)=>{
+      // filter ile tüm laureates elemanlarında sorgu yapıyoruz.
+      // year bilgisi prizes dizisinde olduğu için some fonksiyonundan yararlandık
+      var result=laureates.filter(l=>l.prizes.some(p=>p.year==year));
+      //console.log(result);
+      return result;
+    }
+  },
+  Mutation:{ // CUD operasyonlarının ele alındığı nesne
+    // Yeni bir nobel sahibi eklemek için
+    // input parametresinin şeması schema.graphql içindeki createLaurate nesnesinde belirtildi
+    createLaureate:(parent,input, context,info)=>{
+      //console.log(input);
+      input.prizes=[];
+      laureates.push(input);
+      return input;
+    },
+    // ödül ekleyen metodumuz
+    addPrize:(parent,input,context,info)=>{
+      //console.log(input.year);
+      var laureate=laureates.find(l=>l.id===input.id);
+      //console.log(laureate);
+      if(laureate)
+      {
+        laureate.prizes.push(input);
+        return laureate;
+      }
+      else{
+        return {};
+      }      
+    },
+    deleteLaureate:(parent,{id},context,info)=>{
+      // önce sileceğimiz kayıt var mı indeksini bulalım
+      var index=laureates.findIndex(l=>l.id===id);
+      if(index!=-1) //bulunduysa
+      {
+        //çıkart
+        var deleted=laureates.splice(index,1);
+        return deleted[0];
+      }
+      else
+        return {};
     }
   }
 };
