89f70d0db70e1321b9b9fd17cd058fa7c6acc10a
buraksenyurt <burakselimsenyurt@gmail.com>
2019-03-07 13:47:33 +0300

    angular'da tahmin oyunu örneğine başlandı

---
+++ No 30 - Create Simple Game with Angular/README.md
@@ -0,0 +1,30 @@
+# Angular'ı Biraz Daha İyi Tanımak için Basit Bir Tahmin Oyunu Yazmak
+
+Angular tarafını yavaş yavaş tanımaya başlıyorum. Ancak bilgilerimi pekiştirmek için farklı tuturoial'ları çalışmaya devam etmem gerekiyor. Bu kez temelleri basit şekilde anlamak adına çok basit bir şehir tahmin oyunu yazmaya karar verdim. Uygulama rastsal bir hava durumuna göre kullanıcısına bir soru soracak.
+
+"Merhaba Burak. Bugün hava oldukça "güneşli" ve ben kendimi bir yere ışınladım. Neresi olduğunu tahmin edebilir misin?"
+
+"güneşli" yazan kısım rastgele gelecek bir kelime. Yağmurlu olabilir, soğuk olabilir vb...Buna göre uygun şehirlerden rastgele birisine gidecek bilgisayar. Biz de bunu tahmin etmeye çalışacağız.
+
+>Örnek sonbaharını yaşana WestWorld _(Ubuntu 18.04, 64bit)_ üzerinde geliştirilmektedir.
+
+## Ön Gereksinimler ve Kurulumlar
+
+Sistemde angular CLI yüklü olursa iyi olur. Sonrasında boilerplate etkisi ile uygulamayı oluşturabiliriz. Arayüzün şık görünmesini kolaylaştırmak için _(ben ne kadar şıklaştırabilirsem artık :D )_ bootstrap'i de yüklüyoruz.
+
+```
+sudo npm install -g @angular/cli
+ng new where-am-i --inlineTemplate
+cd where-am-i
+npm install bootstrap --save
+```
+
+## Yapılan Değişiklikler
+
+- Bootstrap'i kullanabilmek için proje klasöründeki angular.json dosyasındaki styles elementinde ilave yapıldı.
+- src/app klasöründeki app.component.html dosyası değiştirildi.
+- src/app klasöründeki app.component.ts typescript dosyasındaki bileşen sınıfı değiştirildi.
+
+## Çalışma Zamanı
+
+## Neler Öğrendim
---
+++ No 30 - Create Simple Game with Angular/where-am-i/src/app/app.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Şimdi Hangi Şehirdeyim?';
+  currentWeather: string; // Güncel hava durumu bilgisini tutan property
+  computersLocation: string; //Bilgisayarın yerini tutacak property
+  playersGuess: string; // Oyuncunun tahminini tutacak property
+  tryCount: number; // Oyuncunun deneme sayısını tuttuğumuz property
+  guessIsCorrect:boolean; // Tahminin doğru olup olmadığını tuttuğumuz property
+
+  // Örnek veri dizileri. 
+  // TODO: Daha uygun bir key-value dizisi bulunabilir mi?
+
+  airConditions = ['güneşli', 'yağmurlu', 'karlı', 'sisli'];
+  cities = [
+    ['Barcelona', 'Madrid', 'Lima', 'Rio', 'Miami', 'Sydney', 'Antalya'],
+    ['Prag', 'Paris', 'Tokyo', 'Dublin', 'Londra', 'Pekin'],
+    ['Moskova', 'Montreal', 'Boston', 'Ağrı'],
+    ['London', 'Glasgow', 'Mexico City', 'Frankfurt', 'İstanbul']
+  ];
+
+  /*
+  Uygulama button bağımsız ilk başlatıldığında da hava tahmini yapılsın ve şehir tutulsun.
+  */
+  constructor() {
+    this.fullThrottle();
+  }
+  /*
+  Bilgisayar için rastgele hava durumu üreten fonksiyon
+  Random fonksiyonundan yararlanıp uygun aralıklarda rastgele sayı üretir
+  ve buna göre rastgele bir şehir tutar.
+  */
+  fullThrottle() {
+    // hava durumlarını tutan dizinin boyutuna göre rastgele sayı ürettik
+    var rnd1 = Math.floor((Math.random() * this.airConditions.length));
+    // rastgele bir hava durumu bilgisi aldık
+    this.currentWeather = this.airConditions[rnd1];
+
+    // şehirlerin tutulduğu dizide, hava durumu bilgisine uyan (örnekte indeks sırası) dizinin uzunluğunu aldık
+    var arrayLength = this.cities[rnd1].length;
+    // uzunluğuna göre rastgele bir sayı ürettik
+    var rnd2 = Math.floor((Math.random() * arrayLength));
+    // üretilen rastgele sayıya göre diziden bir şehir adı aldık
+    this.computersLocation = this.cities[rnd1][rnd2];
+
+    console.log(this.computersLocation); // Şşşşttt. Kimseye söylemeyin. F12'ye basınca ışınlanan şehri görebilirsiniz.
+  }
+
+  /*
+  Oyuncunun tahminini kontrol eden fonksiyon
+  */
+  checkGuess() {
+    this.tryCount++; // Oyuncunun tahmin sayısını bir arttırdık
+  }
+}
